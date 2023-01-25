5eed21d3eadb6dbec8bf66ba5219b5fbda1cc210
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2023-01-25 19:31:32 +0800

    feat: add many stat func

--- src/main.py
+++ src/main.py
@@ -5,8 +5,19 @@ import requests
 
 
 class NSI:
+    def __init__(self):
+        root_path = os.path.join(os.getcwd(), "..", "data", "nsi.min.json")
+        flag_data_exist = True
+        if os.path.exists(root_path) != True:
+            flag_data_exist = False
+        if flag_data_exist != True:
+            self.download_data(root_path)
+        min_json = json.loads(open(root_path, "r", encoding="utf-8").read())
+        self.meta = min_json["_meta"]
+        self.nsi = min_json["nsi"]
+
     def download_data(
-        root_path: str, version="latest", endpoint="jsdeliver"
+        self, root_path: str, version="latest", endpoint="jsdeliver"
     ) -> str:
         PACKAGE_NAME = "name-suggestion-index"
         FILE_PATH = "/dist/nsi.min.json"
@@ -41,17 +52,42 @@ class NSI:
             min_json_file.close()
         return root_path
 
-    def __init__(self):
-        root_path = os.path.join(os.getcwd(), "..", "data", "nsi.min.json")
-        flag_data_exist = True
-        if os.path.exists(root_path) != True:
-            flag_data_exist = False
-        if flag_data_exist != True:
-            self.download_data(root_path)
-        min_json = json.loads(open(root_path, "r", encoding="utf-8").read())
-        self.meta = min_json["_meta"]
-        self.nsi = min_json["nsi"]
+    def get_meta(self, output=True):
+        if output==True:
+            from pprint import pprint
+            pprint(self.meta)
+        return self.meta
+
+    def get_catagory(self, output=True):
+        catagory_list=[]
+        for key in self.nsi:
+            catagory_len=len(self.nsi[key])
+            catagory_list.append((key,catagory_len))
+        if output==True:
+            from pprint import pprint
+            pprint(catagory_list)
+        return catagory_list
+
+    def get_catagory_key(self, output=True):
+        catagory_key_list=[]
+        for key in self.nsi:
+            catagory_key_list.append(key)
+        if output == True:
+            from pprint import pprint
+            pprint(catagory_key_list)
+        return catagory_key_list
+
+
+
+    def get_catagory_num(self, output=False):
+        catagory_num=len(self.get_catagory_key(output=False))
+        if output==True:
+            print(catagory_num)
+        return catagory_num
 
 
 if __name__ == "__main__":
     data = NSI()
+    data.get_meta()
+    data.get_catagory()
+    print(data.get_catagory_num())
