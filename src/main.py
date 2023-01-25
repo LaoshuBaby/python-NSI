64fd40c0bb19ccec166d9c26be752dc61dd2c59b
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2023-01-25 18:49:05 +0800

    feat: add file exist check to save traffic

--- src/main.py
+++ src/main.py
@@ -1,30 +1,51 @@
-flag_data_exist=False
-
 import os
+
 import requests
 
-def download_data(version:str,endpoint="jsdeliver")->str:
-    PACKAGE_NAME="name-suggestion-index"
-    FILE_PATH="/dist/nsi.min.json"
-    root_path=""
+flag_data_exist = True
 
-    endpoint_list={
-        "jsdeliver":"https://cdn.jsdelivr.net/npm/",
-        "unpkg":"https://unpkg.com/"
+
+def download_data(
+    root_path: str, version="latest", endpoint="jsdeliver"
+) -> str:
+    PACKAGE_NAME = "name-suggestion-index"
+    FILE_PATH = "/dist/nsi.min.json"
+
+    endpoint_list = {
+        "jsdeliver": "https://cdn.jsdelivr.net/npm/",
+        "unpkg": "https://unpkg.com/",
     }
-    
-    def url_gen(endpoint:str, version:str)->str:
-        return endpoint_list[endpoint]+PACKAGE_NAME+"@"+version+FILE_PATH
-    
-    min_json_file=open("nsi.min.json","w",encoding="utf-8")
-    min_json_file.write(requests.get(url=url_gen(endpoint=endpoint,version=version)).text)
 
+    def url_gen(endpoint: str, version: str) -> str:
+        return (
+            endpoint_list[endpoint] + PACKAGE_NAME + "@" + version + FILE_PATH
+        )
+
+    if os.path.exists(os.path.join(os.getcwd(), "..", "data")) != True:
+        os.mkdir(os.path.join(os.getcwd(), "..", "data"))
+        min_json_file = open(
+            file=root_path,
+            mode="w",
+            encoding="utf-8",
+        )
+        min_json_file.write(
+            requests.get(
+                url=url_gen(endpoint=endpoint, version=version),
+                headers={"User-Agent": "python-NSI/0.0.1"},
+            ).text
+        )
+        min_json_file.close()
     return root_path
 
+
 def main():
+    root_path = os.path.join(os.getcwd(), "..", "data", "nsi.min.json")
+    if os.path.exists(root_path) != True:
+        flag_data_exist = False
     if flag_data_exist != True:
-        download_data("latest")
+        download_data(root_path)
     exit()
+
 
 if __name__ == "__main__":
     main()
